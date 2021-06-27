b69c54994c9b28f2e6d70c7a2a2bfcc21d36d0e1
Ray <i@r-ay.cn>
2021-06-27 13:21:36 +0800

    新生准备 init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,13 +10,25 @@ module.exports = {
         docsRepo: 'ENDsoft233/51book',
         docsBranch: 'master',
         editLinks: true,
-        editLinkText: '帮我们一起完善 ',
+        editLinkText: '帮我们一起完善',
         lastUpdated: '上次修改 ',
         nav: [
             { text: '返回 五十一中官网', link: 'https://www.wz51z.com/' }
         ],
         sidebar: [
-            ['/', '起始页']
+            ['/', '开始之前'],
+            {
+                title: '新生准备',
+                path: '/新生准备/',
+                sidebarDepth: 1,
+                children: [
+                  "/新生准备/",
+                  '/新生准备/寝室生活',
+                  '/新生准备/学习生活',
+                  '/新生准备/文化生活'
+                ]
+            },
+            ['/知识百科/', '知识百科']
         ],
         smoothScroll: true
     },
