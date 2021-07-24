6bd27e9b220965e15027ee60810d2f8da1465f49
Ray <i@r-ay.cn>
2021-07-24 10:57:43 +0800

    :bug: fatal error in there

--- add_github_contributors.js
+++ add_github_contributors.js
@@ -12,7 +12,7 @@ async function main () {
     let avatarRtn = await axios.get(contributor.avatar_url, { responseType: "arraybuffer" })
     fs.writeFileSync('./docs/.vuepress/public/github-avatars/' + contributor.login + '.svg', `<svg viewBox="0 0 460 460" width="24" height="24" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><image xlink:href="data:image/png;base64,${avatarRtn.data.toString('base64')}" /></svg>`)
     fs.writeFileSync('./docs/README.md', fs.readFileSync('./docs/README.md').toString() + `\n\n- <img style="height: 24px;float: left;margin-right: 10px;border-radius: 15px;" src="./.vuepress/public/github-avatars/${contributor.login}.svg"> [${contributor.login}](${contributor.html_url}) - ${contributor.contributions} 次贡献.`)
-    fs.writeFileSync('./docs/README.md', fs.readFileSync('./docs/联系我们.md').toString() + `\n\n- <img style="height: 24px;float: left;margin-right: 10px;border-radius: 15px;" src="./.vuepress/public/github-avatars/${contributor.login}.svg"> [${contributor.login}](${contributor.html_url}) - ${contributor.contributions} 次贡献.`)
+    fs.writeFileSync('./docs/联系我们.md', fs.readFileSync('./docs/联系我们.md').toString() + `\n\n- <img style="height: 24px;float: left;margin-right: 10px;border-radius: 15px;" src="./.vuepress/public/github-avatars/${contributor.login}.svg"> [${contributor.login}](${contributor.html_url}) - ${contributor.contributions} 次贡献.`)
   }
 
 
