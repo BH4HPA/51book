3bff2523fe22c112977b0a8064a2711930b4cef7
Ray <i@r-ay.cn>
2021-07-25 01:48:55 +0800

    :bug: sitemap 生成 404 尝试解决

--- add_sitemap.js
+++ add_sitemap.js
@@ -3,6 +3,7 @@ const axios = require('axios')
 
 async function main () {
   console.log('start to generate a sitemap by tools.bugscaner.com..')
+  await axios.post('http://tools.bugscaner.com/sitemapspider', 'url=https%3A%2F%2Fwww.wz51z.wiki%2F&encoding=utf-8&make=automation&agent=firefox&pinlv=weekly&youxianji=0.3&filter=1&unicode=1')
   let r = await axios.get('http://tools.bugscaner.com/sitemapspider/download?type=xml&host=www.wz51z.wiki&make=automation&pinlv=weekly&youxianji=0.3&filter=1&filter1=0&unicode=1')
   if (r.status === 200) {
     fs.writeFileSync('./docs/.vuepress/dist/sitemap.xml', r.data)
